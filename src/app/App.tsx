17e44d56f5d9921110c731106af9b860bae37092
edsonhancale <110174899+edsonhancale@users.noreply.github.com>
2023-07-20 08:59:40 +0200

    feat: added recoilroot wrapper

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,12 +4,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import "react-select/dist/react-select.css";
 import { Router } from "../components/routes"
 import "../assets/style/globalStyle.css"
+import { RecoilRoot } from 'recoil';
 
 function App() {
     return (
-        <>
+        <RecoilRoot>
             <Router />
-        </>
+        </RecoilRoot>
     )
 }
 export default App
