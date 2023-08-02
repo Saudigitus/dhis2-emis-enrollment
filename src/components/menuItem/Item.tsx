f8c251c784b7f50dfb377410a30f021cd7f7cde2
edsonhancale <110174899+edsonhancale@users.noreply.github.com>
2023-08-02 15:52:09 +0200

    fix: fixed bug when change values in the sidebar header

--- src/components/menuItem/Item.tsx
+++ src/components/menuItem/Item.tsx
@@ -4,27 +4,26 @@ import { type MenuItemTypes } from '../../types/menu/MenuItemTypes'
 import { paramsMapping } from '../../utils/commons/paramsMapping';
 import { useParams } from '../../hooks/commons/useQueryParams';
 import { useRecoilState } from 'recoil';
-import { type HeaderFieldsSchema, HeaderFieldsState } from '../../schema/headersSchema';
+import { HeaderFieldsState } from '../../schema/headersSchema';
 
 export default function Item({ menuItems, dataElementId, onToggle }: { menuItems: MenuItemTypes[], dataElementId: string, onToggle: () => void }): React.ReactElement {
     const { add } = useParams();
     const [headerFields, setHeaderFields] = useRecoilState(HeaderFieldsState)
-    console.log(menuItems);
 
     const onChange = (selectedOption: { name: string, code: string }) => {
         add(paramsMapping[dataElementId], selectedOption.code);
-        const cloneHeader: HeaderFieldsSchema = { ...headerFields }
+        let dataElements: string[][] = [...headerFields.dataElements]
+        const attributes: string[][] = [...headerFields.attributes]
 
-        const index = cloneHeader.dataElements?.findIndex(x => x.includes(dataElementId))
+        const index = dataElements?.findIndex(x => x?.toString()?.includes(dataElementId))
+
         if (index !== -1) {
-            cloneHeader.dataElements[index] = [`${dataElementId}:in:${selectedOption.code}`]
+            dataElements[index] = [`${dataElementId}:in:${selectedOption.code}`]
         } else {
-            cloneHeader.dataElements?.length > 0
-                ? cloneHeader.dataElements?.push([`${dataElementId}:in:${selectedOption.code}`])
-                : cloneHeader.dataElements = [`${dataElementId}:in:${selectedOption.code}`]
+            dataElements = [...dataElements, [`${dataElementId}:in:${selectedOption.code}`]]
         }
 
-        setHeaderFields(cloneHeader);
+        setHeaderFields({ attributes, dataElements });
         onToggle()
     }
 
