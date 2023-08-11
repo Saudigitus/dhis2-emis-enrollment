28f87be0f309dc86407884fe60b4e72ad96ef2c6
edsonhancale <110174899+edsonhancale@users.noreply.github.com>
2023-08-11 12:54:33 +0200

    fix: fixed bug when load application without any parameters in the url

--- src/hooks/initialValues/useGetInitialValues.ts
+++ src/hooks/initialValues/useGetInitialValues.ts
@@ -6,18 +6,20 @@ import { useLocation } from "react-router-dom";
 export function useGetInitialValues() {
     const location = useLocation()
     const setHeaderFields = useSetRecoilState(HeaderFieldsState)
-    const entries = location.search.split('?')[1].split('&').map((item) => item.split('='))
+    const entries = location?.search?.split('?')?.[1]?.split('&')?.map((item) => item.split('='))
     const dataElementsQuerybuilder = []
-    for (const [key, value] of entries) {
-        const keys = Object.entries(paramsMapping)
-        for (const [dataElement, name] of keys) {
-            if (name.includes(key)) {
-                dataElementsQuerybuilder.push(`${dataElement}:in:${value.replace("+", " ")}`)
+    if (entries?.length > 0) {
+        for (const [key, value] of entries) {
+            const keys = Object.entries(paramsMapping)
+            for (const [dataElement, name] of keys) {
+                if (name.includes(key)) {
+                    dataElementsQuerybuilder.push(`${dataElement}:in:${value.replace("+", " ")}`)
+                }
             }
         }
+        setHeaderFields({
+            attributes: [],
+            dataElements: dataElementsQuerybuilder
+        })
     }
-    setHeaderFields({
-        attributes: [],
-        dataElements: dataElementsQuerybuilder
-    })
 }
