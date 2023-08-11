eb066ebdf2e63e8e5d65cf9e96b95d2d97b95f87
CelsoDeCarvalho <celsomomade361@gmail.com>
2023-08-11 15:05:10 +0200

    feat(component): Add autocomplete component form validation and save socio-economic enpty event [EMISWEB-62]

--- src/components/genericFields/fields/SingleSelect.tsx
+++ src/components/genericFields/fields/SingleSelect.tsx
@@ -3,6 +3,10 @@ import { Autocomplete } from "@material-ui/lab";
 import React from "react";
 import { useField, type FieldRenderProps } from "react-final-form";
 import { type CustomAttributeProps } from "../../../types/table/AttributeColumns";
+import ErrorIcon from '@material-ui/icons/Error';
+import styles from "./fields.module.css"
+import { onSubmitClicked } from "../../modal/ModalContent";
+import { useRecoilState } from "recoil";
 interface AutoCompleteProps {
   disabled?: boolean
   options?: CustomAttributeProps["options"]
@@ -10,7 +14,8 @@ interface AutoCompleteProps {
 }
 
 const OptionSetAutocomplete = (props: AutoCompleteProps) => {
-  const { input, meta }: FieldRenderProps<any, HTMLElement> = useField(props.name);
+  const { input }: FieldRenderProps<any, HTMLElement> = useField(props.name);
+  const [cliked] = useRecoilState<boolean>(onSubmitClicked);
 
   const options = (props?.options?.optionSet?.options != null)
     ? props?.options.optionSet?.options.map((option: { value: string, label: string }) => ({
@@ -20,33 +25,42 @@ const OptionSetAutocomplete = (props: AutoCompleteProps) => {
     : [];
 
   return (
-    <Autocomplete
-      {...props}
-      options={options}
-      closeIcon={null}
-      disabled={props.disabled}
-      getOptionLabel={(option) => option.label}
-      getOptionSelected={(option, value) => option.value === value.value}
-      value={options.find((element: { value: string }) => element.value === input.value) ?? null}
-      renderInput={(params) => (
-        <TextField
-          {...params}
-          variant="outlined"
-          error={(meta.touched === true) && meta.error}
-          helperText={(meta.touched === true) && meta.error}
-          size="small"
-          InputProps={{
-            ...params.InputProps,
-            style: {
-              backgroundColor: "#fff"
-            }
-          }}
-        />
-      )}
-      onChange={(_, value) => {
-        input.onChange(value?.value);
-      }}
-    />
+    <div className={styles["auto-complete__container"]}>
+      <Autocomplete
+        {...props}
+        options={options}
+        closeIcon={null}
+        className={styles["auto-complete__component"]}
+        disabled={props.disabled}
+        getOptionLabel={(option) => option.label}
+        getOptionSelected={(option, value) => option.value === value.value}
+        value={options.find((element: { value: string }) => element.value === input.value) ?? null}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            variant="outlined"
+            error={cliked && input.value === "" && props?.required}
+            helperText={(cliked && input.value === "" && props?.required) && "Please provide a value"}
+            size="small"
+            InputProps={{
+              ...params.InputProps,
+              style: {
+                backgroundColor: "#fff"
+              }
+            }}
+          />
+
+        )}
+        onChange={(_, value) => {
+          input.onChange(value?.value);
+        }}
+      />
+      {
+        (cliked && input.value === "" && props?.required) && <div className={styles["alert-icon__area"]}>
+          <ErrorIcon />
+        </div>
+      }
+    </div>
   );
 };
 
--- src/utils/commons/formatDistinctValue.ts
+++ src/utils/commons/formatDistinctValue.ts
@@ -1,7 +1,7 @@
 export const reducer = (array: any[]) => {
     return array.reduce(function (r, a) {
-        if (a.value !== undefined) {
-            r[a.programStage] = (r[a.programStage]) || [];
+        r[a.programStage] = (r[a.programStage]) || [];
+        if (a.id && a.value) {
             r[a.programStage].push({ dataElement: a.id, value: a.value });
         }
         return r;
