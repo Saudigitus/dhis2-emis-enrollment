fa70e3e119dac30240de894b8b91f46668e077ba
AlziraXerinda <alziraxerinda0@gmail.com>
2023-10-26 16:50:42 +0200

    Added program status when fetching events

--- src/hooks/tableData/useTableData.ts
+++ src/hooks/tableData/useTableData.ts
@@ -16,6 +16,7 @@ interface EventQueryProps {
     ouMode: string
     program: string
     order: string
+    programStatus: string
     programStage: string
     orgUnit: string
     filter?: string[]
@@ -31,13 +32,14 @@ interface TeiQueryProps {
     order: string
 }
 
-const EVENT_QUERY = ({ ouMode, page, pageSize, program, order, programStage, filter, orgUnit, filterAttributes }: EventQueryProps) => ({
+const EVENT_QUERY = ({ ouMode, page, pageSize, program, order, programStage, filter, orgUnit, filterAttributes, programStatus }: EventQueryProps) => ({
     results: {
         resource: "tracker/events",
         params: {
             order,
             page,
             pageSize,
+            programStatus,
             ouMode,
             program,
             programStage,
@@ -117,6 +119,7 @@ export function useTableData() {
                 pageSize,
                 program: getDataStoreData?.program as unknown as string,
                 order: "createdAt:desc",
+                programStatus: "ACTIVE",
                 programStage: getDataStoreData?.registration?.programStage as unknown as string,
                 filter: headerFieldsState?.dataElements,
                 filterAttributes: headerFieldsState?.attributes,
