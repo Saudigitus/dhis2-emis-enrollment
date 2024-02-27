c0704005dfea411cc59b01f2aad806a1a4094c03
AlziraXerinda <alziraxerinda0@gmail.com>
2024-02-27 12:29:17 +0200

    [EMISWEB-110] improvement: Show displayName instead of code in the table

--- src/utils/table/rows/getDisplayNameByOption.tsx
+++ src/utils/table/rows/getDisplayNameByOption.tsx
@@ -1,12 +1,14 @@
 import { defaultProps } from "../../../types/utils/FormatRowsDataProps";
 
 export function getDisplayName({ attribute, value, headers }: defaultProps): string {
-    // for (let i = 0; i < headers.length; i++) {
-    //     if (attribute === headers[i].id && typeof headers[i].optionSets !== 'undefined') {
-    //         for (const op of headers[i].optionSets || []) {
-    //             if (op.code === value) return op.displayName
-    //         }
-    //     }
-    // }
+    if (headers.length > 0) {
+        for (const iterator of headers) {
+            if (iterator.id === attribute && typeof iterator?.options?.optionSet !== 'undefined') {
+                for (const op of iterator?.options?.optionSet?.options || []) {
+                    if (op.value === value) return op.label
+                }
+            }
+        }
+    }
     return value
 }
