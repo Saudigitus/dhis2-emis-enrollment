be8bfb497da37386939422bd8efb2e6486378f0f
cnandza <clayd.nandza@saudigitus.org>
2024-03-14 15:52:06 +0200

    EMISWEB-123 enhance: force the academic year selectection

--- src/layout/fullLayout/FullLayout.tsx
+++ src/layout/fullLayout/FullLayout.tsx
@@ -1,16 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import style from "../Layout.module.css"
-import { MainHeader, SideBar } from '../../components'
 import { CenteredContent, CircularLoader } from "@dhis2/ui";
 import { LayoutProps } from '../../types/layout/LayoutProps';
-import { useGetInitialValues, useGetProgramConfig } from '../../hooks';
+import { InfoPage, MainHeader, SideBar } from '../../components'
+import { useGetInitialValues, useGetProgramConfig, useParams } from '../../hooks';
 import { getDataStoreKeys } from '../../utils/commons/dataStore/getDataStoreKeys';
 
 export default function FullLayout(props: LayoutProps) {
     useGetInitialValues()
     const { children } = props;
+    const { urlParamiters, add } = useParams()
+    const { academicYear, school } = urlParamiters()
     const { isSetSectionType } = useGetInitialValues()
     const { loading } = useGetProgramConfig();
+    const { currentAcademicYear } = getDataStoreKeys()
+
+    useEffect(() => {
+        if ((academicYear === null || academicYear === undefined) || (typeof academicYear === "string" && academicYear?.length === 0)) {
+            add("academicYear", currentAcademicYear)
+        }
+    }, [academicYear])
+
 
     if (!isSetSectionType) {
         return (
@@ -33,7 +43,11 @@ export default function FullLayout(props: LayoutProps) {
             <SideBar />
             <div className={style.FullLayoutContainer}>
                 <MainHeader />
-                <main className={style.MainContentContainer}>{children}</main>
+                <main className={style.MainContentContainer}>
+                    {
+                        (school === null || school === undefined) ? <InfoPage /> : children
+                    }
+                </main>
             </div>
         </div>
     )
