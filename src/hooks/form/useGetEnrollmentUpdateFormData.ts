32b521d16a0f8a35153e50091a998dfb7add0392
cnandza <clayd.nandza@saudigitus.org>
2024-03-14 17:00:45 +0200

    EMISWEB-125 fix: error when updating event without dataValues fixed

--- src/hooks/form/useGetEnrollmentUpdateFormData.ts
+++ src/hooks/form/useGetEnrollmentUpdateFormData.ts
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { format } from 'date-fns';
 import { useRecoilValue } from 'recoil';
-import { useGetTei, useGetEvent, useParams } from '../../hooks';
+import { useGetTei, useGetEvent, useParams, useShowAlerts } from '../../hooks';
 import { getSelectedKey } from '../../utils/commons/dataStore/getSelectedKey';
 import { attributes, dataValues } from '../../utils/table/rows/formatResponseRows';
 import { HeaderFieldsState } from '../../schema/headersSchema';
@@ -10,6 +10,7 @@ export default function useGetEnrollmentUpdateFormData () {
     const { getTei } = useGetTei()
     const { getEvent } = useGetEvent()
     const {  urlParamiters } = useParams()
+    const { hide, show } = useShowAlerts()
     const { school : orgUnit } = urlParamiters()
     const { getDataStoreData } = getSelectedKey()
     const [enrollmentValues, setEnrollmentValues] = useState<any>({})
@@ -17,7 +18,7 @@ export default function useGetEnrollmentUpdateFormData () {
     const [initialValues, setInitialValues ] =  useState<any>({})
     const headerFieldsState = useRecoilValue(HeaderFieldsState)
 
-    const buildFormData =  (trackedEntity: string ) => {
+    const buildFormData =  (trackedEntity: string, enrollmentId: string) => {
         setLoading(true)
         if (Object.keys(getDataStoreData)?.length) {
             const { registration, 'socio-economics': { programStage }, program } = getDataStoreData
@@ -44,15 +45,37 @@ export default function useGetEnrollmentUpdateFormData () {
                                 setEnrollmentValues({
                                     trackedEntity: trackedEntityInstance?.results?.instances[0],
                                     events: [
-                                        registration?.results?.instances[0] ?? {},
-                                        socioEconomic?.results?.instances[0] ?? {}
+                                        registration?.results?.instances[0] ?? 
+                                        {...trackedEntityInstance?.results?.instances[0]?.enrollments?.find((enrollment: any) => enrollment.enrollment === enrollmentId).events?.find((event: any) => event.programStage === registration.programStage), enrollment: enrollmentId},
+
+                                        socioEconomic?.results?.instances[0] ?? 
+                                        {...trackedEntityInstance?.results?.instances[0]?.enrollments?.find((enrollment: any) => enrollment.enrollment === enrollmentId)?.events?.find((event: any) => event.programStage === programStage),enrollment: enrollmentId},
                                     ]
                                 })
+                            
+                                setLoading(false)
+                            })
+                            .catch((error) => {
+                                show({
+                                    message: `${("Could not get data")}: ${error.message}`,
+                                    type: { critical: true }
+                                });
                                 setLoading(false)
                             })
                     })
+                    .catch((error) => {
+                        show({
+                            message: `${("Could not get data")}: ${error.message}`,
+                            type: { critical: true }
+                        });
+                        setLoading(false)
+                    })
             })
-            .catch(() => {
+            .catch((error) => {
+                show({
+                    message: `${("Could not get data")}: ${error.message}`,
+                    type: { critical: true }
+                });
                 setLoading(false)
             })
         }
--- src/utils/events/formatPostBody.ts
+++ src/utils/events/formatPostBody.ts
@@ -3,12 +3,13 @@ import { reducer } from "../commons/formatDistinctValue";
 export const eventUpdateBody = (enrollmentsData: any[], events: any[], enrollmentDate: any,  formValues:any, orgUnit: string, programId: string, trackedEntity: string) => {
     const form : any = []
 
-
     for (const enrollmentData of enrollmentsData) {
         if (enrollmentData[0].type === "dataElement") {
             for (const [key, value] of Object.entries(reducer(enrollmentData))) {
+
                 const event = events.find((event: any) => event.programStage === key)
-                if(event !== undefined)
+
+                if(Object.keys(event).length > 4)
                     form.push({
                         ...event,
                         occurredAt: enrollmentDate,
@@ -18,11 +19,13 @@ export const eventUpdateBody = (enrollmentsData: any[], events: any[], enrollmen
                 else 
                     form.push({
                         notes: [],
-                        orgUnit,
+                        orgUnit:orgUnit,
                         status: "ACTIVE",
                         programStage: key,
                         program: programId,
-                        trackedEntity,
+                        trackedEntity:trackedEntity,
+                        enrollment: event.enrollment,
+                        event: event.event,
                         occurredAt: enrollmentDate,
                         scheduledAt: enrollmentDate,
                         dataValues: returnEventDataValues(enrollmentData, formValues)
