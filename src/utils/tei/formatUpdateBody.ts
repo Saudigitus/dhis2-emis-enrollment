996eb73d506a5c98ef525fa4d6a01183c66f8ed0
cnandza <clayd.nandza@saudigitus.org>
2024-04-22 14:09:56 +0200

    add missing key to update enrollment body

--- src/utils/tei/formatUpdateBody.ts
+++ src/utils/tei/formatUpdateBody.ts
@@ -35,7 +35,7 @@ export const teiUpdateBody = (enrollmentData: any[], orgUnit: string, trackedEnt
                     createdAt: format(new Date(enrollmentDate), "yyyy-MM-dd'T'HH:mm:ss.SSS"),
                     occurredAt: format(new Date(enrollmentDate), "yyyy-MM-dd'T'HH:mm:ss.SSS"),
                     enrolledAt: format(new Date(enrollmentDate), "yyyy-MM-dd'T'HH:mm:ss.SSS"),
-                    ...eventUpdateBody(enrollmentData, events, enrollmentDate, formValues, orgUnit, program, trackedEntity)
+                    events: eventUpdateBody(enrollmentData, events, enrollmentDate, formValues, orgUnit, program, trackedEntity)
                 }
             ]
         }
