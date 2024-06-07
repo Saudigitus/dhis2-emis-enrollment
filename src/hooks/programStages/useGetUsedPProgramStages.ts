82d428ee47db56849791045cea3ed2d1a55eaf50
AlziraXerinda <alziraxerinda0@gmail.com>
2024-06-07 10:12:34 +0200

    fix: Send final result programstage if there is no performance key on datastore object

--- src/hooks/programStages/useGetUsedPProgramStages.ts
+++ src/hooks/programStages/useGetUsedPProgramStages.ts
@@ -4,6 +4,6 @@ import { getDataStoreKeys } from "../../utils/commons/dataStore/getDataStoreKeys
 const useGetUsedPProgramStages = () => {
     const { performance, finalResult } = getDataStoreKeys();
     const performanceProgramStages = performance?.programStages.map((programStage: simpleProgramStage) => programStage.programStage) ?? [];
-    return performance ? [...performanceProgramStages, finalResult?.programStage] : []
+    return performance ? [...performanceProgramStages, finalResult?.programStage] : [ finalResult?.programStage ]
 }
 export default useGetUsedPProgramStages
