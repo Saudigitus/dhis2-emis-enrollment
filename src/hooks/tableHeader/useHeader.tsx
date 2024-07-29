aa397d462fdec432552ad4916a2491c22738678e
cnandza <clayd.nandza@saudigitus.org>
2024-07-29 09:01:50 +0200

    EMISWEB-174 hide dataElements columns from table dinamicaly based on viewport width

--- src/hooks/tableHeader/useHeader.tsx
+++ src/hooks/tableHeader/useHeader.tsx
@@ -3,13 +3,15 @@ import { ProgramConfigState } from "../../schema/programSchema";
 import { formatResponse } from "../../utils/table/header/formatResponse";
 import { getSelectedKey } from "../../utils/commons/dataStore/getSelectedKey";
 import { TableColumnState } from "../../schema/columnSchema";
+import useViewportWidth from "../rwd/useViewportWidth";
 
 export function useHeader() {
     const tableColumns = useRecoilValue(TableColumnState)
     const programConfigState = useRecoilValue(ProgramConfigState);
     const { getDataStoreData } = getSelectedKey()
+    const { viewPortWidth } = useViewportWidth()
 
     return {
-        columns: formatResponse(programConfigState, getDataStoreData, tableColumns),
+        columns: formatResponse(programConfigState, getDataStoreData, tableColumns, viewPortWidth),
     }
 }
